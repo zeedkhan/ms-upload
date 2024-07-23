cea9f5ffbebc7bcfef937669261f7763dd5925ca
zeedkhan <68377219+zeedkhan@users.noreply.github.com>
2024-07-23 02:35:05 +0000

    add backend

--- src/utils/server.ts
+++ src/utils/server.ts
@@ -2,6 +2,9 @@ import express, { Express, Request, Response } from "express"
 import path from 'path';
 import uploadRouter from "../routes/upload";
 import cors from "cors"
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const createServer = () => {
     const app: Express = express();
